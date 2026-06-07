e39e5feb016b4f62992d15ae946bc4f7c21b7bb5
makedoni <aleximlord@gmail.com>
2026-06-07 13:05:10 +0300

    fix: remove stale eslint-disable directives in tfjs-webgl-stub

--- src/lib/classifier/tfjs-webgl-stub.ts
+++ src/lib/classifier/tfjs-webgl-stub.ts
@@ -3,15 +3,10 @@
 // Small text-classification models have no perceptible perf difference on CPU.
 
 export const version_webgl = '0.0.0';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const GPGPUContext = class {} as any;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const MathBackendWebGL = class {} as any;
 export const forceHalfFloat = () => {};
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const gpgpu_util = {} as any;
 export const setWebGLContext = () => {};
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const webgl = {} as any;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const webgl_util = {} as any;
